a19bbffe92dcb1d7b5618679e99ad2b47c6e604a
JhonatanAragao <ajhonatamslash@gmail.com>
2023-03-06 21:14:34 -0300

    .

---
+++ medidor_de_velocidade.py
@@ -0,0 +1,32 @@
+"""
+Levando em consideração a velocidade máxima permitida de 80Km em uma determinada rua. Crie um programa que recebe
+do usuário um valor que representa a velocidade e com base nessa velocidade diga se ele levou uma multa leve, grave
+ou gravissíma. Levando em consideração que se a pessoa estiver abaixo da velocidade máxima seu programa deve
+exibir "não houve multa", caso esteja até 10Km acima, deve exibir: "multa leve",
+entre 11 a 20Km : multa grave
+e acima de 20Km da velocidade máxima, exiba multa gravissíma.
+"""
+
+"""
+1 - Quais são os dados de entrada necessários?
+R= velocidade em Km do usuário
+
+2 - O que devo fazer com estes dados?
+R = Fazer a comparação por meio de condicionais e exibir o resultado com base nos requisitos e valor passado pelo usuário.
+
+3 - Quais são as restrições para este problema?
+R = O usuário não pode passar uma velocidade igual a zero, número negativo e ou em forma de texto.
+
+4 - Qual o resultado esperado?
+R = Espera-se que o programa mostre o grau da multa para o usuário, com base na sua velocidade informada.
+
+5 - Qual é a sequência de passos a serem feitas para chegar ao resultado esperado?
+one - solicitar o valor da velocidade ao usuário.
+two - armazenar esse valor em uma variável.
+three - verificar se o valor repassado pelo usuário é um valor válido.
+four - solicitar novamente o valor caso o mesmo repassado seja inválido.
+five - fazer o comparativo através de condicionais entre o valor repassado e os requisitos na ementa.
+six - mostrar ao usuário o grau da sua multa, caso tenha. Caso contrário, informa-lo que não houve multa.
+seven - finalizar programa > program finish
+
+"""
---
+++ pixels_e_o_sistema_rgb.py
@@ -0,0 +1,7 @@
+print("olá mundo")
+
+print("\ncara agora eu vou somar 5 + 2 e te mostrar o resultado")
+a = 5
+b = 2
+s = 5 + 2
+print(s)
